2782bf2798185db50b9dee27a06967fb153bc904
alessandrobianchi <alessandro.bianchi231@gmail.com>
2018-03-20 00:04:28 +0100

    Fixed null values import (drop)

--- Code/Alessandro/one_lang_corpus_saver.py
+++ Code/Alessandro/one_lang_corpus_saver.py
@@ -6,15 +6,19 @@ ARCHIVE_FILENAME = "../../../data_not_committed/Archive/" + LANGUAGE + ".csv"
 BRANDS_CORPUS_FILENAME = "../../../data_not_committed/CSE_20180215+KateTokyo_" + LANGUAGE + "_texts.csv"
 MERGED_CORPUS_FILENAME = "../../../data_not_committed/all_" + LANGUAGE + "_texts.npy"
 
-
-df_archive = pd.read_csv(ARCHIVE_FILENAME, encoding='utf-8', dtype=str)
+# Read archive df dropping all rows which have a NaN value
+df_archive = pd.read_csv(ARCHIVE_FILENAME, encoding='utf-8', dtype=str, nrows=2).dropna(axis=0, how='any')
 archive_corpus = df_archive.post_message.values
 
-df_brands = pd.read_csv(BRANDS_CORPUS_FILENAME, encoding='utf-8', dtype=str)
+# Read brands df dropping all rows which have a NaN value
+df_brands = pd.read_csv(BRANDS_CORPUS_FILENAME, encoding='utf-8', dtype=str).dropna(axis=0, how='any')
 brands_corpus = df_brands.text.values
 
-# TODO Check for duplicated values before saving
+# Concatenate the two corpus
+concat_corpus = np.concatenate((brands_corpus, archive_corpus))
 
-corpus = np.concatenate((brands_corpus, archive_corpus))
+# Drop duplicates, if any
+corpus_df = pd.DataFrame(concat_corpus, columns=['text']).drop_duplicates()
 
-np.save(MERGED_CORPUS_FILENAME, corpus)
+# Save to numpy file
+np.save(MERGED_CORPUS_FILENAME, corpus_df.text.values)
